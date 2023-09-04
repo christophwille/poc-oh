bb53b18ed29792f4f17dc0b306b522382f92b771
Christoph Wille <christoph.wille@gmail.com>
2023-09-04 08:13:14 +0200

    TypeNameHandling.All sample

---
+++ src/JSON/TypedMessageSerialization/Program.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+
+var settings = new JsonSerializerSettings
+{
+    TypeNameHandling = TypeNameHandling.All,
+};
+
+var container = new MessageContainer()
+{
+    Id = Guid.NewGuid().ToString(),
+    Type = typeof(ASimpleMessage).Name,
+    Data = JsonConvert.SerializeObject(
+        new ASimpleMessage("Hello World", "Everywhere"),
+        settings)
+};
+
+Console.WriteLine(container.Data);
+var data = JsonConvert.DeserializeObject<IMessageMarker>(container.Data, settings);
+
+Console.WriteLine((data as ASimpleMessage).Message);
+
+public class MessageContainer
+{
+    public string Id { get; set; }
+    public string Type { get; set; }
+    public string Data { get; set; }
+}
+
+public interface IMessageMarker { };
+
+public record ASimpleMessage(string Message, string Location) : IMessageMarker;
