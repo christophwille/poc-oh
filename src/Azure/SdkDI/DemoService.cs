0b18de6ceb373b5ce6cdd2fe73cd731428915f78
Christoph Wille <christoph.wille@gmail.com>
2023-09-11 16:11:04 +0200

    Draft sample for Azure SDK dependency injection

---
+++ src/Azure/SdkDI/DemoService.cs
@@ -0,0 +1,17 @@
+﻿using Azure.Security.KeyVault.Secrets;
+using Azure.Storage.Blobs;
+
+namespace SdkDI
+{
+    public class DemoService
+    {
+        private readonly SecretClient _secretClient;
+        private readonly BlobServiceClient _blobServiceClient;
+
+        public DemoService(SecretClient secretClient, BlobServiceClient blobServiceClient)
+        {
+            _secretClient = secretClient;
+            _blobServiceClient = blobServiceClient;
+        }
+    }
+}
---
+++ src/Azure/SdkDI/Extensions.cs
@@ -0,0 +1,47 @@
+﻿using Azure.Identity;
+using Microsoft.Extensions.Azure;
+
+namespace SdkDI
+{
+    public static class Extensions
+    {
+        private const string BlobUri = "https://{0}.blob.core.windows.net";
+
+        // https://learn.microsoft.com/en-us/dotnet/azure/sdk/dependency-injection
+        public static void AddSdkClients(this IServiceCollection services, IConfiguration configuration, bool isDevelopmentEnvironment = false)
+        {
+            services.AddAzureClients(clientBuilder =>
+            {
+                clientBuilder
+                    .AddBlobServiceClient(new Uri(String.Format(BlobUri, "aname")))
+                    .WithName("aname");
+
+                //clientBuilder
+                //    .AddBlobServiceClient(configuration...) // connectionstring-based
+                //    .WithName("aname");
+
+                string keyVaultUrl = configuration.GetValue<string>("akeyvaulturlsetting");
+                clientBuilder.AddSecretClient(new Uri(keyVaultUrl));
+
+                // Basic options https://blog.jongallant.com/2021/08/azure-identity-201/
+                // Optimize via https://scottsauber.com/2022/05/10/improving-azure-key-vault-performance-in-asp-net-core-by-up-to-10x/
+                // Ordered resolution via ChainedTokenCredential: https://yourazurecoach.com/2020/08/13/managed-identity-simplified-with-the-new-azure-net-sdks/
+                Azure.Core.TokenCredential credential = null;
+                if (isDevelopmentEnvironment)
+                {
+                    credential = new VisualStudioCredential();
+                }
+                else
+                {
+                    credential = new ChainedTokenCredential(
+                            new ManagedIdentityCredential(),
+                            new VisualStudioCredential(),
+                            new AzurePowerShellCredential()
+                        );
+                }
+
+                clientBuilder.UseCredential(credential);
+            });
+        }
+    }
+}
