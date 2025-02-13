700e2decc24e2808a520977470b6539813c40084
Christoph Wille <christoph.wille@gmail.com>
2025-02-13 15:51:40 +0100

    Try IHostBuilder outside container (fails)

---
+++ src/TestcontainersPlayground/TestcontainersPlayground.AzFunc/02_OnlyDepsInContainers.cs
@@ -0,0 +1,96 @@
+﻿using AzFunc.ToTest;
+using Microsoft.Azure.Functions.Worker.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.VisualBasic;
+
+namespace TestcontainersPlayground;
+
+public class FunctionAppFixture : IAsyncLifetime
+{
+    private readonly IHost _host;
+
+    public IServiceProvider ServiceProvider => _host.Services;
+
+    /*
+    [0]: "--host"
+    [1]: "127.0.0.1"
+    [2]: "--port"
+    [3]: "65107"
+    [4]: "--workerId"
+    [5]: "7a22e78c-32f2-4e6e-8acd-c01a41c52647"
+    [6]: "--requestId"
+    [7]: "87a6eb21-3625-41a6-bd31-4f3f7d491565"
+    [8]: "--grpcMaxMessageLength"
+    [9]: "2147483647"
+    [10]: "--functions-uri"
+    [11]: "http://127.0.0.1:65107/"
+
+    [12]: "--functions-worker-id"
+    [13]: "7a22e78c-32f2-4e6e-8acd-c01a41c52647"
+    [14]: "--functions-request-id"
+    [15]: "87a6eb21-3625-41a6-bd31-4f3f7d491565"
+    [16]: "--functions-grpc-max-message-length"
+    [17]: "2147483647"  */
+    public FunctionAppFixture()
+    {
+        // TODO: It is being ignored...
+        var builder = FunctionsApplication.CreateBuilder([
+            "--host", "127.0.0.1",
+            "--port", "65107",
+            "--functions-uri", "http://127.0.0.1:65107/"
+            ]);
+        builder.ConfigureFunctionsWebApplication();
+
+        builder.Configuration
+            .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+            .AddEnvironmentVariables();
+
+        // https://github.com/mawax/azure-functions-integration-testing/blob/main//ExampleFunctionApp/ExampleFunctionApp.Integration.Tests/HostBuilderExtensions.cs#L20
+        // Otherwise, this exception on _host.StartAsync:
+        // Collection fixture type 'TestcontainersPlayground.FunctionAppFixture' threw in InitializeAsync
+        // ----System.InvalidOperationException : Configuration is missing the 'HostEndpoint' information.Please ensure an entry with the key 'Functions:Worker:HostEndpoint' is present in your configuration. 
+        var hostedService = builder.Services.First(descriptor => descriptor.ImplementationType?.Name == "WorkerHostedService");
+        builder.Services.Remove(hostedService);
+
+        _host = builder.Build();
+
+        // HERE: Azurite and MsSql Testcontainers
+    }
+
+    public async ValueTask InitializeAsync()
+    {
+        await _host.StartAsync();
+    }
+    public async ValueTask DisposeAsync()
+    {
+        _host?.Dispose();
+    }
+}
+[CollectionDefinition("FunctionApp")]
+public class FunctionAppCollection : ICollectionFixture<FunctionAppFixture>
+{
+}
+
+[Collection("FunctionApp")]
+public class Approach2Tests : IClassFixture<FunctionAppFixture>
+{
+    private readonly FunctionAppFixture _fixture;
+
+    public Approach2Tests(FunctionAppFixture fixture)
+    {
+        _fixture = fixture;
+    }
+
+    [Fact]
+    public async Task Request_ReturnsResponseWithServerTimingHeader()
+    {
+        // One thing for sure: we are IHostBuilder Isolated v2 (that is a difference to all other older approaches)
+        // Is this maybe related to MTS being no longer IsTestProject and output being .exe with in-built Main?
+
+        var sut = ActivatorUtilities.CreateInstance<SampleHttpFunctionNoDeps>(_fixture.ServiceProvider);
+
+        // sut.Run(new HttpRequest())
+    }
+}
--- src/TestcontainersPlayground/TestcontainersPlayground.AzFunc/README.md
+++ src/TestcontainersPlayground/TestcontainersPlayground.AzFunc/README.md
@@ -1,6 +1,9 @@
 # Testing Azure Functions
 
-## Approach 1
+The problem: [Implement test worker/test framework](https://github.com/Azure/azure-functions-dotnet-worker/issues/281)
+
+
+## Approach 1 (Function in Testcontainers itself)
 
 https://www.tpeczek.com/2023/10/azure-functions-integration-testing.html
 
@@ -8,13 +11,37 @@ with code to be found here
 
 https://github.com/tpeczek/dotnet-server-timing/tree/main/test/Test.Azure.Functions.Worker.ServerTiming
 
+or
+
+https://totheroot.io/article/hands-on-integration-test-azure-functions
+
+
 ## Approach 2
 
-https://github.com/ormikopo1988/azure-functions-isolated-code-testing-sample/blob/main/tests/FunctionApp.IsolatedDemo.Api.Tests.Integration/NotesFunctionTests.cs
-(article similar to that https://medium.com/@yusufsarikaya023/integration-test-for-azure-function-net-core-isolated-worker-47dd63afa0e7 
-or also https://www.wagemakers.net/posts/testing-service-bus-trigger-azure-functions/)
+* All of those are not https://learn.microsoft.com/en-us/azure/azure-functions/dotnet-isolated-process-guide?tabs=hostbuilder%2Cwindows#version-2x
+* All of those are not Microsoft.Testing.Framework with no IsTestProject / .exe output
+
+https://github.com/ormikopo1988/azure-functions-isolated-code-testing-sample/ interesting bits eg TestHelpers.cs
+
+Articles similar to that 
+
+* https://www.wagemakers.net/posts/testing-service-bus-trigger-azure-functions/ [repo](https://github.com/mawax/azure-functions-integration-testing)
+* https://medium.com/@yusufsarikaya023/integration-test-for-azure-function-net-core-isolated-worker-47dd63afa0e7 [repo](https://github.com/yusufsarikaya023/AzureFunctionIntegrationTest)
 
 
 ## Approach 3
 
 https://github.com/wigmorewelsh/FunctionTestHost
+
+That has several issues 
+
+* xUnit2 (which makes it incompatible in a project using xUnit3)
+* Dragging in a lot of stuff eg Orleans (which I understand if you build something to fit your needs)
+* Is using IHostBuilder and not Isolated Functions v2 (FunctionsApplicationBuilder, see also same problem with UnitTestEx)
+
+
+## Aspire and others
+
+https://learn.microsoft.com/en-us/dotnet/aspire/serverless/functions?tabs=dotnet-cli&pivots=visual-studio
+
+https://github.com/Avanade/unittestex?tab=readme-ov-file#HTTP-triggered-Azure-Function (WebJobs code atm)
