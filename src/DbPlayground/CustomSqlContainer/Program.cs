8c76abb6b3060089cbcc5739e8c8a91ac4a2a46b
Christoph Wille <christoph.wille@gmail.com>
2025-04-14 09:25:12 +0200

    WIP Build Your Own SQL Image

---
+++ src/DbPlayground/CustomSqlContainer/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+
+// string connectionString = "Server=.;Database=pubs;Integrated Security=true;Encrypt=True;TrustServerCertificate=True";
+string connectionString = "Server=127.0.0.1,10109;Database=pubs;User Id=sa;Password=YourS3cureP@ass;Encrypt=True;TrustServerCertificate=True";
+string query = "SELECT * FROM authors";
+
+try
+{
+    using (SqlConnection connection = new SqlConnection(connectionString))
+    {
+        connection.Open();
+
+        using (SqlCommand command = new SqlCommand(query, connection))
+        {
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                if (reader.HasRows)
+                {
+                    Console.WriteLine("Authors:");
+                    while (reader.Read())
+                    {
+                        string authorId = reader["au_id"].ToString();
+                        string authorName = reader["au_lname"].ToString();
+                        Console.WriteLine($"ID: {authorId}, Name: {authorName}");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No authors found.");
+                }
+            }
+        }
+    }
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"An error occurred: {ex.Message}");
+}
---
+++ src/DbPlayground/CustomSqlContainer/README.md
@@ -0,0 +1,23 @@
+# Notes
+
+Built based on the article:
+
+https://www.kenmuse.com/blog/devops-sql-server-dacpac-docker
+
+## Create yourself a bacpac
+
+`dotnet tool install --global Microsoft.SqlPackage --version 170.0.86-preview`
+
+Eg 
+
+`SqlPackage /Action:Export /TargetFile:"SampleDb.bacpac" /SourceConnectionString:"Server=.;Integrated Security=SSPI;Initial Catalog=pubs;Encrypt=True;TrustServerCertificate=True;"`
+
+
+## Build the container image
+
+`docker build --build-arg PASSWORD=YourS3cureP@ass --build-arg DBNAME=pubs -t pubs:1.0 .`
+
+## Run the container
+
+`docker run -p 10109:1433 --name pubsdb -d pubs:1.0`
+
