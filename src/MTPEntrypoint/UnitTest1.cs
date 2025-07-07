1717d1e86b46b8a00b20bc241ae17ee69dff5123
Christoph Wille <christoph.wille@gmail.com>
2025-07-07 09:49:12 +0200

    MTP in .NET 10: Find MTPEntrypoint.MicrosoftTestingPlatformEntryPoint

---
+++ src/MTPEntrypoint/UnitTest1.cs
@@ -0,0 +1,36 @@
+﻿namespace MTPEntrypoint
+{
+    public class Tests
+    {
+        // Press F10 to hit the startup class of MTP
+        /*
+        namespace MTPEntrypoint
+        {
+            [global::System.Diagnostics.CodeAnalysis.ExcludeFromCodeCoverage]
+            internal sealed class MicrosoftTestingPlatformEntryPoint
+            {
+                public static async global::System.Threading.Tasks.Task<int> Main(string[] args)
+                {
+                    global::Microsoft.Testing.Platform.Builder.ITestApplicationBuilder builder = await global::Microsoft.Testing.Platform.Builder.TestApplication.CreateBuilderAsync(args);
+                    SelfRegisteredExtensions.AddSelfRegisteredExtensions(builder, args);
+                    using (global::Microsoft.Testing.Platform.Builder.ITestApplication app = await builder.BuildAsync())
+                    {
+                        return await app.RunAsync();
+                    }
+                }
+            }
+        }*/
+
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
