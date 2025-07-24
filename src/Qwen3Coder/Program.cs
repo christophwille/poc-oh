cb83fa911b8dfb069c5afd7b8cb2de3876a1ba6a
Christoph Wille <christoph.wille@gmail.com>
2025-07-24 08:19:12 +0200

    Qwen3(Coder) playground added

---
+++ src/Qwen3Coder/Program.cs
@@ -0,0 +1,10 @@
+﻿using OllamaSharp;
+
+var uri = new Uri("http://localhost:11434");
+var ollama = new OllamaApiClient(uri);
+
+// https://ollama.com/hoangquan456/qwen3-nothink
+ollama.SelectedModel = "hoangquan456/qwen3-nothink:0.6b";
+
+await foreach (var stream in ollama.GenerateAsync("Provide me a C# program that creates an ULID"))
+    Console.Write(stream.Response);
