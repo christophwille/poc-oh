a2c1efd2f47cfcbc4d6394b11ee232e735db0cf2
Christoph Wille <christoph.wille@gmail.com>
2025-08-04 19:18:47 +0200

    C# 14 extension everything tests

---
+++ src/CSharp14ExtensionTests/Program.cs
@@ -0,0 +1,37 @@
+﻿Console.WriteLine("Hello, World!");
+
+internal static class ExtensionProperties
+{
+    extension<T>(ICollection<T> collection) where T : notnull
+    {
+        public bool IsEmpty => collection.Count == 0;
+
+        public int Test
+        {
+            get
+            {
+                return 42;
+            }
+            set
+            {
+            }
+        }
+
+        public void AddIfNotNull(T item)
+        {
+            if (item != null)
+            {
+                collection.Add(item);
+            }
+        }
+
+        public T2 Cast<T2>(int index) where T2 : T
+        {
+            return (T2)(object)collection.ElementAt(index);
+        }
+
+        public static void StaticExtension()
+        {
+        }
+    }
+}
