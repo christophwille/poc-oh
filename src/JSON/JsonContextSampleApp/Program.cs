47b4341c583a8a8d63d450d085b6c57aaf937d7c
Christoph Wille <christoph.wille@gmail.com>
2025-11-05 09:53:11 +0100

    Using JsonSerializerContext sample

---
+++ src/JSON/JsonContextSampleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+var val = JsonSerializer.Deserialize<SampleDataStructure>(@"{""Id"": 42 }", JsonContext.Default.SampleDataStructure);
+
+Console.WriteLine(val.Id);
+
+[JsonSerializable(typeof(SampleDataStructure))]
+public partial class JsonContext : JsonSerializerContext { }
+
+public class SampleDataStructure
+{
+    public int Id { get; set; }
+}
