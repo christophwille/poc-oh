6e1c652c65ba803387cd07f2ec71db0f2932ce9c
Christoph Wille <christoph.wille@gmail.com>
2025-11-05 07:31:50 +0100

    Add note on OIDC challenge, cookie default

--- src/Azure/EntraAuthBlazor10SI/Program.cs
+++ src/Azure/EntraAuthBlazor10SI/Program.cs
@@ -13,13 +13,20 @@ using Microsoft.Identity.Web.UI;
 //     Notable change: FallbackPolicy for Authorization to require authentication globally
 //     Notes: changes req'd to _Imports.razor, RedirectToLogin.razor added, Routes.razor modified, Layout has username/sign out
 // References:
-// https://github.com/dotnet/aspnetcore/issues/51202#issuecomment-1998525088
+// https://github.com/dotnet/aspnetcore/issues/51202#issuecomment-1998525088 (interesting OIDC variations later on)
 // https://dev.to/the_architect/set-up-azure-entra-authentication-with-blazor-server-24mg
 // https://iliaselmatani.codes/posts/azureadauthenticationblazorfluentui/
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+builder.Services
+    .AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    //.AddAuthentication(options =>
+    //{
+    //    // You want cookie as default and OIDC only for challenge
+    //    options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+    //    options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+    //})
     .AddMicrosoftIdentityWebApp(options =>
     {
         builder.Configuration.Bind("AzureAd", options);
