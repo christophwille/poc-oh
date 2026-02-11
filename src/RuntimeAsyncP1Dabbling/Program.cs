5459166f763c711398d3a9a15f318f2ff8f9f26d
Christoph Wille <christoph.wille@gmail.com>
2026-02-11 16:48:01 +0100

    Runtime async with net11p1

---
+++ src/RuntimeAsyncP1Dabbling/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://dotnet.microsoft.com/en-us/download/dotnet/11.0
+// https://github.com/dotnet/core/blob/main/release-notes/11.0/preview/preview1/runtime.md#runtime-async
+
+await WriteToFileAsync("Hello, World!");
+Console.WriteLine("done");
+
+static async Task<bool> WriteToFileAsync(string text)
+{
+    await System.IO.File.WriteAllTextAsync("sample.txt", text);
+    return true;
+}
