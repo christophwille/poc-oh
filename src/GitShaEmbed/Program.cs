9210d9b6343e034a84d219e54f09713a7dde30d7
Christoph Wille <christoph.wille@gmail.com>
2026-03-02 12:16:24 +0100

    Embed git sha in assembly

---
+++ src/GitShaEmbed/Program.cs
@@ -0,0 +1,18 @@
+﻿// https://github.com/devlooped/GitInfo unusable because of license requirements (as usual)
+
+// https://github.com/adamralph/minver
+// https://github.com/dotnet/Nerdbank.GitVersioning
+// https://github.com/GitTools/GitVersion go-to tool when full versioning is required
+
+// Lighweight
+// https://gist.github.com/ElanHasson/30384431651534b215e754a554b5b6ae
+// https://github.com/MarkPflug/MSBuildGitHash
+
+using System.Reflection;
+
+string? gitHash = Assembly
+    .GetEntryAssembly()!
+    .GetCustomAttributes<AssemblyMetadataAttribute>()
+    .FirstOrDefault(attr => attr.Key == "GitHash")?.Value;
+
+Console.WriteLine($"Git hash: {gitHash}");
