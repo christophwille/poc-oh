8e681920ddbee3e08065baad7776ca46779f8991
Christoph Wille <christoph.wille@gmail.com>
2026-03-14 09:12:44 +0100

    coverlet.MTP for NUnit

---
+++ src/MTPCoverletNUnit/MTPCoverlet.Tests/SampleClassTests.cs
@@ -0,0 +1,24 @@
+﻿using AwesomeAssertions;
+using NUnit.Framework;
+
+namespace MTPCoverlet.Tests
+{
+    public class SampleClassTests
+    {
+        // For why the csproj looks like it does, see:
+        // https://learn.microsoft.com/en-us/dotnet/core/testing/microsoft-testing-platform-migration-from-v1-to-v2#compatibility-with-vstest-based-dotnet-test
+        // https://github.com/coverlet-coverage/coverlet/issues/1827
+
+        // Options https://www.nuget.org/packages/coverlet.MTP/#readme-body-tab
+        // eg dotnet test --coverlet
+        // coverage data in: MTPCoverlet.Tests\bin\Debug\net10.0\TestResults
+
+        [Test]
+        public void MyTest()
+        {
+            var sut = new SampleClass();
+            var result = sut.SampleMethod(true);
+            result.Should().Be("Branch1");
+        }
+    }
+}
---
+++ src/MTPCoverletNUnit/MTPCoverlet/SampleClass.cs
@@ -0,0 +1,17 @@
+﻿namespace MTPCoverlet
+{
+    public class SampleClass
+    {
+        public string SampleMethod(bool yesMam)
+        {
+            if (yesMam)
+            {
+                return "Branch1";
+            }
+            else
+            {
+                return "Branch2";
+            }
+        }
+    }
+}
