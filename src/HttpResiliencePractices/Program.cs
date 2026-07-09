8f97c864f482eceb675c5a318ca20f25d1b788b7
Christoph Wille <christoph.wille@gmail.com>
2026-07-09 09:51:00 +0200

    Sample for http resilience handler (default and custom override)

---
+++ src/HttpResiliencePractices/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Http.Resilience;
+using Polly;
+
+// Based on https://milanjovanovic.tech/blog/overriding-default-http-resilience-handlers-in-dotnet
+// with https://github.com/dotnet/extensions/pull/5801 added (see csproj for NoWarn)
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+builder.Services
+    .AddHttpClient()
+    .ConfigureHttpClientDefaults(http => http.AddStandardResilienceHandler());
+
+builder.Services
+    .AddHttpClient("github")
+    .ConfigureHttpClient(client =>
+    {
+        client.BaseAddress = new Uri("https://api.github.com");
+    })
+    .RemoveAllResilienceHandlers()
+    .AddResilienceHandler("custom", pipeline =>
+    {
+        pipeline.AddTimeout(TimeSpan.FromSeconds(10));
+
+        pipeline.AddRetry(new HttpRetryStrategyOptions
+        {
+            MaxRetryAttempts = 3,
+            BackoffType = DelayBackoffType.Exponential,
+            UseJitter = true,
+            Delay = TimeSpan.FromMilliseconds(500)
+        });
+
+        pipeline.AddTimeout(TimeSpan.FromSeconds(1));
+    });
+
+
+using IHost host = builder.Build();
+await host.RunAsync();
